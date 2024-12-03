356f2aec8233ece5900ed0ef4ac1c9835fd0d300
Rafael Carvalho <rafaelhenriquecarvalho@outlook.com>
2024-12-02 22:18:13 -0300

    Feat: Add Absolute, Grid and Flex layouts

--- AppMAUIGallery/Repositories/ICategoryRepository.cs
+++ AppMAUIGallery/Repositories/ICategoryRepository.cs
@@ -44,6 +44,18 @@ namespace AppMAUIGallery.Repositories
                             Title = "Grid",
                             Description = "Organiza os elementos dentro de uma tabela.",
                             Page = typeof(GridLayoutPage)
+                        },
+                        new Component
+                        {
+                            Title = "AbsoluteLayout",
+                            Description = "Liberdade total para posicionar e dimensionar os elementos na tela",
+                            Page = typeof(AbsoluteLayoutPage)
+                        },
+                        new Component
+                        {
+                            Title = "FlexLayout",
+                            Description = "Organiza os elementos de forma sequencial com muitas opções de personalização",
+                            Page = typeof(FlexLayoutPage)
                         }
                     }
                 }
