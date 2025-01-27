1a6ff0ca3403b365d73c9aa30324c585a8ef8e5e
Rafael <rafael.carvalho@danysoft.net>
2025-01-27 08:00:48 -0300

    Feat: add Models EFC in project

---
+++ AppTask.Models/TaskModel.cs
@@ -0,0 +1,19 @@
+﻿namespace AppTask.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public DateTime PrevisionDate { get; set; }
+        public bool IsCompleted { get; set; }
+        public required List<SubTaskModel> SubTasks { get; set; }
+
+        public class SubTaskModel 
+        {
+            public int Id { get; set; }
+            public required string Name { get; set; }
+            public bool IsCompleted { get; set; }
+        }
+    }
+}
