fff2843fb7bb6c58a369b2a7de1b5ce957aae944
Rafael <rafael.carvalho@danysoft.net>
2025-03-14 13:54:11 -0300

    Feat: Criando o AppMVVMCommunity

--- AppMVVMCommunityToolkit/App.xaml.cs
+++ AppMVVMCommunityToolkit/App.xaml.cs
@@ -1,12 +1,13 @@
-﻿namespace AppMVVMCommunityToolkit
+﻿using AppMVVMCommunityToolkit.Views;
+
+namespace AppMVVMCommunityToolkit
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
-
-            MainPage = new AppShell();
+            MainPage = new StartPage();
         }
     }
 }
--- AppMVVMCommunityToolkit/Models/Person.cs
+++ AppMVVMCommunityToolkit/Models/Person.cs
@@ -8,6 +8,8 @@ namespace AppMVVMCommunityToolkit.Models
 {
     public class Person
     {
-
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
     }
 }
