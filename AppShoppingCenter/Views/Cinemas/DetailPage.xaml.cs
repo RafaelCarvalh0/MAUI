f9e2c797a18bef560e67b8485116c52a86e8bf35
Rafael <rafael.carvalho@danysoft.net>
2025-06-05 14:09:49 -0300

    Feat: Add player de video and animation

--- AppShoppingCenter/Views/Cinemas/DetailPage.xaml.cs
+++ AppShoppingCenter/Views/Cinemas/DetailPage.xaml.cs
@@ -2,8 +2,30 @@ namespace AppShoppingCenter.Views.Cinemas;
 
 public partial class DetailPage : ContentPage
 {
-	public DetailPage()
-	{
-		InitializeComponent();
-	}
+    bool firstTime = false;
+    public DetailPage()
+    {
+        InitializeComponent();
+    }
+
+    private void PlayPause(object sender, TappedEventArgs e)
+    {
+        if(firstTime is false)
+        {
+            btn_playpause.TranslateTo(- player.Width / 2, player.Width / 2, 500);
+            firstTime = true;
+        }
+
+        if (player.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Playing)
+        {
+            player.Pause();
+            btn_playpause.Source = ImageSource.FromFile("play.png");
+        }
+
+        else
+        {
+            player.Play();
+            btn_playpause.Source = ImageSource.FromFile("pause.png");
+        }
+    }
 }
